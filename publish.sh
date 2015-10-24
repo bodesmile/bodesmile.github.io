e61c7e0bb8a573d7339b497d2a9ae1c88f91b21e
Bode_MBP15 <bodesmile@me.com>
2015-10-24 12:09:07 +0800

    更新

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 cd /Users/Bode/Documents/MyBlog@Github/source
+git push origin source
 jekyll build
 cd /Users/Bode/Documents/MyBlog@Github/blog
 rm -r ./*
 cp -r /Users/Bode/Documents/MyBlog@Github/source/_site/* ./
 cp /Users/Bode/Documents/MyBlog@Github/source/.gitignore ./
-git add -A
+git add .
 git commit -m "更新"
 git push origin master
 cd /Users/Bode/Documents/MyBlog@Github/source
