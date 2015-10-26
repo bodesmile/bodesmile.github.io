91e50c1ae61c3f662c8564d11f2d181ad537566c
Bode_MBP15 <bodesmile@me.com>
2015-10-26 20:52:26 +0800

    删除冗余代码

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
+NOTE=$1
+
 cd /Users/Bode/Documents/MyBlog@Github/source
 echo "更新代码"
 git add .
-git commit -m "更新source"
+git commit -m ${NOTE}
 git push origin source
 echo "准备发布"
 jekyll build
@@ -10,9 +12,11 @@ rm -r ./*
 cp -r /Users/Bode/Documents/MyBlog@Github/source/_site/* ./
 cp /Users/Bode/Documents/MyBlog@Github/source/.gitignore ./
 git add .
-git commit -m "更新"
+git commit -m ${NOTE}
 echo "正在上传"
 git push origin master
 cd /Users/Bode/Documents/MyBlog@Github/source
 
-echo "DONE!!!!"
+echo "更新并上传"
+echo ${NOTE}
+echo "已完成"
